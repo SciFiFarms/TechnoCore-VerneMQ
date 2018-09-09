e34f9f6f5f835861c9ef442db8f20b3e3865b4ee
Spencer Hachmeister <SpencerHachmeister@gmail.com>
2018-09-09 16:01:28 -0600

    Correct out of date mapi (MQTT API) paths.

--- dogfish/shell-migrations/shared/utils.sh
+++ dogfish/shell-migrations/shared/utils.sh
@@ -16,12 +16,17 @@ extract_from_json(){
     grep -Eo '"'$1'":.*?[^\\]"' <<< "$2" | cut -d \" -f 4
 }
 
-# $1: The secret name (Without stack reference, this will be delt with on portainer's end.)
+# $1: The mount point(Without stack reference, this will be delt with on portainer's end.)
+#   Should be of the format service_name/mount_point
+#   If the swarm service is different than the service name used to save the 
+#   secret, then use secret_service_name&swarm_service_name. For example:
+#   portainer/secret/create/node-red&nr/mqtt_username
 # $2: The secret itself
 function create_secret()
 {
-    mosquitto_pub -h mqtt -p 8883 -q 1 \
-        -t portainer/secrets/create/$1 \
+    mosquitto_pub -i create_secret_client -h mqtt -p 8883 -q 1 \
+        -r \
+        -t portainer/secret/create/$1 \
         -m "$2" \
         -u $(cat /run/secrets/mqtt_username) \
         -P "$(cat /run/secrets/mqtt_password)" \
