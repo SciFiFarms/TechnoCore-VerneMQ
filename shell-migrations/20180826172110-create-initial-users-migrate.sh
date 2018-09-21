c3c9a9cc0b6677c31c2df08254d4a83893f73ded
Spencer Hachmeister <SpencerHachmeister@gmail.com>
2018-09-21 16:39:31 -0700

    Add delay to migration that creates initial users.

--- shell-migrations/20180826172110-create-initial-users-migrate.sh
+++ shell-migrations/20180826172110-create-initial-users-migrate.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+#echo "Running middle migration."
+
+# TODO: Find a better way to check that the server is up and remove the sleep. 
+# The issue is that vernemq ping works as soon as the server starts to load, long before 
+# the server is actually ready to ready to handle these requests. However, I 
+# haven't found a good replacement as at this stage, the credentials needed to
+# log in haven't been set up yet. Moving to key based authentication could solve this. 
+until vernemq ping
+do
+    echo "Couldn't reach MQTT. Sleeping."
+    sleep 1
+done
+sleep 10
+#echo "Finished wait for middle migration."
+
 add_user_to_vernemq $(cat /run/secrets/portainer_mqtt_username) $(cat /run/secrets/portainer_mqtt_password)
 add_user_to_vernemq $(cat /run/secrets/mqtt_username) $(cat /run/secrets/mqtt_password)
 
