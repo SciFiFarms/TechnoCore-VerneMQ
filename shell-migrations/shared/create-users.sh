12c35a9d4ef18b331fbde848f1e86a66ceba1c2c
Spencer Hachmeister <SpencerHachmeister@gmail.com>
2018-09-26 20:02:16 -0600

    Move create_user and main user creation migration to portainer.

--- shell-migrations/shared/create-users.sh
+++ shell-migrations/shared/create-users.sh
@@ -27,24 +27,3 @@ EOF
     #echo "user $1\rtopic \r" >> /etc/vernemq/vmq.acl
 
 }
-
-# $1: The username to create
-# $2: The service this user is for
-function create_user()
-{
-    # TODO: I think there is a programatic way to do this that would allow for 
-    # max retries annd sleep time to be passed in as parameters. 
-    until local response=$(curl --cacert /run/secrets/ca \
-        -H "X-Vault-Token: $(cat /run/secrets/token)" \
-        -X POST \
-        https://vault:8200/v1/sys/tools/random/32);
-    do
-        echo "Can't reach Vault, sleeping."
-        sleep 5
-    done;
-    mqtt_password=$(extract_from_json random_bytes "$response")
-
-    add_user_to_vernemq "$1" "$mqtt_password"
-    create_secret "$2/mqtt_username" "$1"
-    create_secret "$2/mqtt_password" "$mqtt_password"
-}
