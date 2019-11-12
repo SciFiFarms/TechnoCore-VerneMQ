227a170ef81003f27fb97a7073c7c64560c9c8fb
Spencer Hachmeister <SpencerHachmeister@gmail.com>
2019-11-11 19:19:22 -0700

    Add create_secrets service to listen to MQTT and create secrets.

--- secrets.sh
+++ secrets.sh
@@ -9,3 +9,5 @@ source ${TECHNOCORE_LIB}/create-secret.sh
 # The exception is if there is an admin username/password. In that case, ${STACK_NAME}_admin... does 
 # not get created because there is no admin service.
 #generate_password_for [your_service_name]
+
+generate_password_for create_secrets
